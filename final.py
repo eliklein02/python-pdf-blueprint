7662a625f335698671d69fe065e1eed283edea56
Eli Klein <eliklein02@gmail.com>
2025-02-26 17:35:15 -0500

    driver

--- final.py
+++ final.py
@@ -16,12 +16,15 @@ import shutil
 # from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# # from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.core.os_manager import ChromeType
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.service import Service
 
 from urllib.parse import quote
 from urllib.parse import urlparse
@@ -177,16 +180,21 @@ def extract_googleapis_link(url):
     # if chrome_path is None:
     #     raise Exception("Chromedriver not found")
     
-    service = Service(executable_path="./chromedriver")
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
+    # service = Service(executable_path="./chromedriver")
+    # options = Options()
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--no-sandbox")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1200')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
     caps = DesiredCapabilities.CHROME
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
     options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-    driver = webdriver.Chrome(service=service, options=options)
+    # driver = webdriver.Chrome(service=service, options=options)
     print(driver)
     try:
         payload = {}
